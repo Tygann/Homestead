2d8f17822b6ce01cf65d31e1f8ff03da21568941
Tyler <Tyler@Keegan.me>
2026-07-16 15:41:25 -0500

    Refresh Settings sheet accent color

--- Homestead/App/ContentView.swift
+++ Homestead/App/ContentView.swift
@@ -88,6 +88,7 @@ struct ContentView: View {
                         }
                     }
             }
+            .accentColor(Color(appearanceSettings.appColor.uiColor))
             .preferredColorScheme(settingsSheetColorScheme)
             }
         }
